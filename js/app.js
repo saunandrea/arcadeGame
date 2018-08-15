b70db19636ee382ee4f421bb32224640fad09733
Andrea Saunders <saunandrea@gmail.com>
2018-08-14 21:37:16 -0600

    fixing formatting

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ Creating several new Enemies objects and placing them in an array called allEnem
 
 
 // Enemies our player must avoid
-function Enemy(x,y,speed)  {
+function Enemy(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -50,7 +50,7 @@ function Enemy(x,y,speed)  {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -68,39 +68,39 @@ Enemy.prototype.update = function(dt) {
     let enemyYMin = this.y + 80;
     let enemyXMax = this.x + 101;
     let enemyXMin = this.x + 5;
-    
+
     let yCollision = (((playerYMax > enemyYMin) && (playerYMax < enemyYMax)) || ((playerYMin > enemyYMin) && (playerYMin < enemyYMax)) || (playerYMin < enemyYMin && playerYMax > enemyYMax));
     let xCollision = (((playerXMax > enemyXMin) && (playerXMax < enemyXMax)) || ((playerXMin > enemyXMin) && (playerXMin < enemyXMax)));
 
-    if (yCollision && xCollision){
+    if (yCollision && xCollision) {
         player.sendHome();
     }
 
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-function Player()  {
+function Player() {
     this.sprite = 'images/char-boy.png'; //todo: why won't the other characters work?
     this.x = 205;
     this.y = 340;
 };
 
-Player.prototype.sendHome = function(dt) {
-  //  let hit = 'images/hit.png'; //todo: why won't another image work? wanted to flash stars when hit.
-  //  ctx.drawImage(Resources.get(hit), this.x, this.y);
+Player.prototype.sendHome = function (dt) {
+    //  let hit = 'images/hit.png'; //todo: why won't another image work? wanted to flash stars when hit.
+    //  ctx.drawImage(Resources.get(hit), this.x, this.y);
     this.x = 205;
     this.y = 340;
-   };
-
-Player.prototype.update = function(dt) {
- //todo: not sure what supposed to do.
 };
 
-Player.prototype.handleInput = function(direction) {
+Player.prototype.update = function (dt) {
+    //todo: not sure what supposed to do.
+};
+
+Player.prototype.handleInput = function (direction) {
     let scale = 10;
     if (direction == "left" && (this.x + 18 - scale > 10)) {
         this.x -= scale;
@@ -112,30 +112,30 @@ Player.prototype.handleInput = function(direction) {
             console.log("you wonnnn");
             this.sendHome();
             document.querySelector(".win-container").classList.add('is-visible');
-            setTimeout(function(){ document.querySelector(".win-container").classList.remove('is-visible'); }, 5000);
-            allEnemies.push(new Enemy(Math.random()*300+1,Math.random()*200+1,Math.random()*80+1))
+            setTimeout(function () { document.querySelector(".win-container").classList.remove('is-visible'); }, 5000);
+            allEnemies.push(new Enemy(Math.random() * 300 + 1, Math.random() * 200 + 1, Math.random() * 80 + 1))
         }
     }
-    else if (direction == "right" && (this.x + 84 + scale < (505 - scale)))  {
+    else if (direction == "right" && (this.x + 84 + scale < (505 - scale))) {
         this.x += scale;
     }
     else if (direction == "down" && (this.y + 142 + scale < (600 - scale))) {
         this.y += scale;
     }
-}	
+}
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy(0,60,50),new Enemy(60,120,50),new Enemy(300,180,50)]; //
+let allEnemies = [new Enemy(0, 60, 50), new Enemy(60, 120, 50), new Enemy(300, 180, 50)];
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
